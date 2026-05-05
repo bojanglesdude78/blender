44c82e294215ed7dfada6b917ff8f2768a702993
bojanglesdude78 <132537796+bojanglesdude78@users.noreply.github.com>
2026-05-04 22:02:32 -0400

    Update bevel_operator.py

--- tests/python/bevel_operator.py
+++ tests/python/bevel_operator.py
@@ -8,6 +8,8 @@
 # BLENDER_VERBOSE=1 blender path/to/bevel_regression.blend --python path/to/bevel_operator.py -- --run-test <index>
 # where <index> is the index of the test specified in the list tests.
 
+# hello
+
 import bpy
 import os
 import sys
